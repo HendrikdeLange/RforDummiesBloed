fb154956ba40d4424dd574caf6afe4e99ffd5507
DamienRichards <95588557+DamienRichards@users.noreply.github.com>
2025-02-24 11:08:05 +0200

    R_Array_Functions
    KlasGebak2

---
+++ Functions.R
@@ -0,0 +1,15 @@
+
+
+A1 = array(seq(0.2,1.0, 0.1), dim = c(3, 3))
+A2 = array(seq(1.2,2.0, 0.1), dim = c(3, 3))
+
+A1 
+A2
+
+A1 + A2
+A2 - A1
+A1 / A2
+A1 * A2 #not matrix multiplication
+
+A1 %*% A2 # matrix multiplication syntax
+
